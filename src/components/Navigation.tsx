ca278f403c8dcc6b73e9b99b03a36cf4a056c027
devByByron <youngkillian0308@gmail.com>
2025-09-10 08:51:01 +0200

    Added experience navLink to the navigation bar

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -6,6 +6,7 @@ const navItems = [
   { name: 'Home', href: '#hero' },
   { name: 'About', href: '#about' },
   { name: 'Skills', href: '#skills' },
+  { name: 'Experience', href: '#experience' },
   { name: 'Education', href: '#education' },
   { name: 'Projects', href: '#projects' },
   { name: 'Contact', href: '#contact' },
